92965c3b04e3689d23e76de5ab53e0059b89cf3c
Kerem Bozdas <krmbzds.github@gmail.com>
2016-05-09 16:23:25 +0300

    Add mockup functionality

--- backend/tasks.py
+++ backend/tasks.py
@@ -1,7 +1,9 @@
 from __future__ import absolute_import
 
 from backend.celery import app
+from django.http import JsonResponse
+import random
 
 @app.task(ignore_result=True)
 def predict(review):
-    return 5  # The Spark task should be invoked from here
+    return JsonResponse({"result": random.randint(1,5)})
--- backend/urls.py
+++ backend/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from .tasks import predict
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^predict/', predict)
 ]
